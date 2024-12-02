3feb90ce6270b0ba56bb26d531ba5169e5af0c84
Deccan Maniam <dev.oofsauce@gmail.com>
2024-12-02 12:28:20 -0800

    comments & day-2 (bad but works)

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,11 +1,15 @@
+// everything is a reference and i dont know why
+
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 use std::collections::HashMap;
 
 fn part1(list1: &mut Vec<i32>, list2: &mut Vec<i32>) {
+    // sorting will align the lists
     list1.sort();
     list2.sort();
 
+    // iterate and sum differences
     let mut res: i32 = 0;
     for i in 0..list1.len() {
         let diff = list2[i] - list1[i];
@@ -16,6 +20,7 @@ fn part1(list1: &mut Vec<i32>, list2: &mut Vec<i32>) {
 }
 
 fn part2(list1: &mut Vec<i32>, list2: &mut Vec<i32>) {
+    // create map of frequencies of vals in list2
     let mut freq = HashMap::new(); // i dont specify type, but prob involves "&mut i32"
     for n in list2 {
         if freq.contains_key(&n) { // i dont know what these ampersands are for
@@ -25,11 +30,12 @@ fn part2(list1: &mut Vec<i32>, list2: &mut Vec<i32>) {
         }
     }
 
+    // reference list2 freq map with list1 vals
     let mut res = 0;
     for n in list1 {
         if freq.contains_key(&n) {
             let freq = freq.get(&n).unwrap(); // alternatively, dont unwrap and handle the none case
-            res += *n * freq;
+            res += *n * freq; // have to dereference umbrella "integer" type ig
         }
     }
 
@@ -37,6 +43,7 @@ fn part2(list1: &mut Vec<i32>, list2: &mut Vec<i32>) {
 }
 
 fn main() {
+    // set up file i/o
     let path: &str = "input.txt";
     let input: File = File::open(path).unwrap();
     let buffer: BufReader<File> = BufReader::new(input);
@@ -44,9 +51,13 @@ fn main() {
     // i32... my beloved c++ int...
     let mut list1: Vec<i32> = Vec::new(); // calling a default constructor
     let mut list2: Vec<i32> = Vec::new();
+
     for line in buffer.lines() {
+        // gather string data
         let l = line.unwrap();
         let mut iter = l.split_whitespace();
+
+        // form lists
         list1.push(iter.next().unwrap().parse().expect("invalid num1")); // resolve result to expect parsing error
         list2.push(iter.next().unwrap().parse().expect("invalid num2"));
     }
--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,69 +1,150 @@
 use std::io::{BufReader, BufRead};
 use std::fs::File;
+use std::env;
 
-fn part1() {
-    let path: &str = "input.txt";
-    let input: File = File::open(path).unwrap();
-    let buffer: BufReader<File> = BufReader::new(input);
+fn part1(report: Vec<i32>) -> bool {
+    // determine if report is valid decreasing
+    let mut valid = true;
+    let mut prev_num = report[0];
+    for i in 1..report.len() {
+        let num = report[i];
 
-    let mut safe = 0;
-    for line in buffer.lines() {
-        let l = line.unwrap();
-        let iter = l.split_whitespace();
-        let mut report: Vec<i32> = Vec::new();
-
-        for i in iter {
-            let num = i.parse().expect("invalid number");
-            report.push(num);
+        // if invalid decrease, break
+        if prev_num - num < 1 || prev_num - num > 3 {
+            valid = false;
+            break;
         }
 
-        let mut valid_dec = true;
-        let mut valid_inc = true;
-        let mut prev_num = report[0];
-        for i in 1..report.len() {
-            let num = report[i];
-
-            if prev_num - num < 1 || prev_num - num > 3 {
-                valid_dec = false;
-            }
-
-            if num - prev_num < 1 || num - prev_num > 3 {
-                valid_inc = false;
-            }
-
-            prev_num = num;
-        }
-
-        safe += if valid_inc || valid_dec {1} else {0};
+        prev_num = num;
     }
 
-    println!("{:?}", safe);
-}
-
-fn valid_remove(ind: i32, report: &mut Vec<i32>, inc: bool) -> bool {
-    if(ind == 0 || ind == report.len()-1) {
+    // only need 1 valid condition to be safe
+    if valid {
         return true;
     }
 
-    if(inc) {
-        if(report[ind+1] - report[ind-1] < 1 || report[ind+1] - report[ind-1] > 3) {
+    // determine if report is valid increasing
+    valid = true;
+    prev_num = report[0];
+    for i in 1..report.len() {
+        let num = report[i];
+
+        // if invalid increase, break
+        if num - prev_num < 1 || num - prev_num > 3 {
+            valid = false;
+            break;
+        }
+
+        prev_num = num;
+    }
+
+    return valid;
+}
+
+// check if removing value at index results in valid report (locally) (idiotically written) (should be called "valid_skip")
+fn valid_remove(ind: usize, report: &Vec<i32>, inc: bool) -> bool {
+    // bool differentiates order of subtraction
+    if inc {
+        // check if removing is valid
+        if ind > 0 && ind < report.len()-1 && (report[ind+1] - report[ind-1] < 1 || report[ind+1] - report[ind-1] > 3) {
+            return false;
+        }
+
+        // can cause problems in next number so must check to solve problem locally
+        if ind < report.len()-2 && (report[ind+2] - report[ind+1] < 1 || report[ind+2] - report[ind+1] > 3) {
             return false;
         }
         return true;
     } else {
-        if(report[ind-1] - report[ind+1] < 1 || report[ind-1] - report[ind+1] > 3) {
+        // check if removing is valid
+        if ind > 0 && ind < report.len()-1 && (report[ind-1] - report[ind+1] < 1 || report[ind-1] - report[ind+1] > 3) {
+            return false;
+        }
+
+        // can cause problems in next number so must check to solve problem locally
+        if ind < report.len()-2 && (report[ind+1] - report[ind+2] < 1 || report[ind+1] - report[ind+2] > 3) {
             return false;
         }
         return true;
     }
 }
 
-fn part2() {
+fn part2(report: Vec<i32>) -> bool {
+    // determine if report is valid decreasing
+    let mut valid = true;
+    let mut skip = false;
+    let mut prev_num = report[0];
+    for i in 1..report.len() {
+        let num = report[i];
+
+        if prev_num - num < 1 || prev_num - num > 3 {
+            if skip { // if weve skipped once, we cant skip again
+                valid = false;
+                break;
+            } else {
+                if valid_remove(i-1, &report, false) { // remove left
+                    skip = true;
+                } else if valid_remove(i, &report, false) { // remove right
+                    skip = true;
+                    continue; // so prev_num isn't updated
+                } else { // if no remove works, report is invalid
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        prev_num = num;
+    }
+
+    if valid {
+        return true;
+    }
+
+    // determine if report is valid increasing
+    valid = true;
+    skip = false;
+    prev_num = report[0];
+    for i in 1..report.len() {
+        let num = report[i];
+
+        if num - prev_num < 1 || num - prev_num > 3 {
+            if skip { // if weve skipped once, we cant skip again
+                valid = false;
+                break;
+            } else {
+                if valid_remove(i-1, &report, true) { // remove left
+                    skip = true;
+                } else if valid_remove(i, &report, true) { // remove right
+                    skip = true;
+                    continue; // so prev_num isn't updated
+                } else { // if no remove works, report is invalid
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        prev_num = num;
+    }
+
+    return valid;
+}
+
+fn main() {
+    // collecting cmd args (b/c why not)
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 || (args[1] != "--1" && args[1] != "--2") {
+        println!("Invalid argument.");
+        return;
+    }
+
+    // set up file i/o
     let path: &str = "input.txt";
     let input: File = File::open(path).unwrap();
     let buffer: BufReader<File> = BufReader::new(input);
 
-    let mut safe = 0;
+    let mut safe = 0; // number of safe reports
     for line in buffer.lines() {
         let l = line.unwrap();
         let iter = l.split_whitespace();
@@ -74,35 +155,13 @@ fn part2() {
             report.push(num);
         }
 
-        let mut valid_dec = true;
-        let mut valid_inc = true;
-        let mut prev_num = report[0];
-        let mut skip = false;
-        for i in 1..report.len() {
-            let num = report[i];
-
-            if prev_num - num < 1 || prev_num - num > 3 {
-                if skip {
-                    valid_dec = false;
-                }
-            }
-
-            if num - prev_num < 1 || num - prev_num > 3 {
-                if skip {
-                    valid_inc = false;
-                }
-            }
-
-            prev_num = num;
+        // easy to choose btwn part 1 and part 2
+        if args[1] == "--1" {
+            safe += if part1(report) {1} else {0};
+        } else if args[1] == "--2" {
+            safe += if part2(report) {1} else {0};
         }
-
-        safe += if valid_inc || valid_dec {1} else {0};
     }
 
     println!("{:?}", safe);
-}
-
-fn main() {
-    part1();
-    // part2();
 }
